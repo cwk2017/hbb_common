d10216e27762b3cf650f156478c0496aa2672399
cwk2017 <30595468+cwk2017@users.noreply.github.com>
2025-05-10 11:39:22 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["tb.gxbhcwk.site"];
+pub const RS_PUB_KEY: &str = "nK70uo6NP7MfUj3LYK7ZWdeFyNEKSh81V0JNt8YcyDQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
